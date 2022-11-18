961018215ceb55b588d8e20a33d92037a953bac6
Charles Joma <almprincecharles@yahoo.com>
2022-11-19 10:54:45 +1100

    DONE

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 //
+// TURNS OUT I DONT NEED THIS THERE IS AN ANIMATION FEATURE IN AFRAME
 
 
+// ======= This works but i cant figure oout  ========== //
 
-// ======= DOSENT WORK IN AR.js for me ATM ========== //
-
-// const chepCube = document.getElementById('chep-ball')
+// const chepCube = document.getElementById('chep-cube')
 
 // const animateUp = (object, Zposition) => {
 //     const position = `x:0 , y:${Math.floor(0 + -Math.tan(Date.now()) / 1000)}, `;
     
-//     object.setAttribute('position', {x: 0, y:0 + -Math.tan(Date.now()) / 100, z: 0 });
+
+//     object.setAttribute('position', {x: 0, y:0.6 + Math.tan(Math.random() * -9) / -3, z: 0 });
     
 
 //     requestAnimationFrame(() => animateUp(object, Zposition));
@@ -23,5 +24,5 @@
 
 // animateUp(chepCube, '0');
 // console.log(chepCube.getAttribute('position'))
-// // animateUp(chepBall, '0');
+// animateUp(chepBall, '0');
 
