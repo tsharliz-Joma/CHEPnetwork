1fb35a10de463aa694475896c648cf9a5aef92b8
Charles Joma <almprincecharles@yahoo.com>
2022-11-19 09:03:20 +1100

    Add animate feature to ball

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $ = (querySelector) => document.querySelector(querySelector);
 
 const chepCube = $('chep-cube');
+const chepBall = $('chep-ball');
 
 const animateUp = (object, Yposition) => {
     const position = `0 ${0 + -Math.tan(Date.now() / 1000)} ${Yposition}`;
@@ -8,5 +9,8 @@ const animateUp = (object, Yposition) => {
     requestAnimationFrame(() => animateUp(object, Yposition));
 }
 
+
+
 animateUp(chepCube, '1.75');
+animateUp(chepBall, '1.5');
 
